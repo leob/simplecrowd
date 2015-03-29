88b65a4de8c08886b0cccb06cddf648fb0c6c087
leob <leob@users.noreply.github.com>
2015-03-29 21:20:06 +0200

    Heroku deployment: PostgreSQL doesn't like 'boolean limit 1' so we take that out of schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20150328145417) do
   create_table "categories", force: :cascade do |t|
     t.string   "name_nl",    limit: 255
     t.string   "name_en",    limit: 255
-    t.boolean  "enabled",    limit: 1,   default: true, null: false
+    t.boolean  "enabled",    default: true, null: false
     t.datetime "created_at",                            null: false
     t.datetime "updated_at",                            null: false
   end
@@ -30,9 +30,9 @@ ActiveRecord::Schema.define(version: 20150328145417) do
     t.string   "target_amount_currency",    limit: 255,   default: "EUR", null: false
     t.integer  "collected_amount_cents",    limit: 4,     default: 0,     null: false
     t.string   "collected_amount_currency", limit: 255,   default: "EUR", null: false
-    t.boolean  "draft",                     limit: 1,     default: false, null: false
-    t.boolean  "disabled",                  limit: 1,     default: false, null: false
-    t.boolean  "editor_pick",               limit: 1,     default: false, null: false
+    t.boolean  "draft",                     default: false, null: false
+    t.boolean  "disabled",                  default: false, null: false
+    t.boolean  "editor_pick",               default: false, null: false
     t.datetime "created_at",                                              null: false
     t.datetime "updated_at",                                              null: false
     t.string   "image_file_name",           limit: 255
@@ -52,8 +52,8 @@ ActiveRecord::Schema.define(version: 20150328145417) do
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "",    null: false
     t.string   "encrypted_password",     limit: 255, default: "",    null: false
-    t.boolean  "admin",                  limit: 1,   default: false, null: false
-    t.boolean  "locked",                 limit: 1,   default: false, null: false
+    t.boolean  "admin",                  default: false, null: false
+    t.boolean  "locked",                 default: false, null: false
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
