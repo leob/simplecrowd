4375ec9992f26ff2ece24a3a2cc32381297732b7
leob <leob@users.noreply.github.com>
2015-04-13 00:35:00 +0200

    small bugfix: project block layout under OSX/Safari (bug report Richard 12 april 2015)

--- sc_switch_with_public.sh
+++ sc_switch_with_public.sh
@@ -9,18 +9,19 @@ then
     cp gitignore_with_public .gitignore
    
     rm -rf public/fonts public/images public/javascripts public/stylesheets
+    git merge --no-commit master
     grunt build-prod
+
+    git status
 
     echo "ON BRANCH: 'with_public'."
 
-    echo "Directory 'public' was cleaned and 'grunt build-prod' was executed."
+    echo "Directory 'public' was cleaned, changes were merged from master, and 'grunt build-prod' was executed."
 
-    echo "DON'T FORGET TO DO: 'git merge --no-commit master' !!"
-
-    echo After that you can do: 'gc -m "updated public folder for heroku"'
+    echo DONT FORGET TO DO: 'gc -m "updated public folder for heroku"'
     echo and: 'git push heroku with_public:master'
 
-    echo "WHEN FINISHED DON'T FORGET TO INVOKE: 'sc_switch_master.sh' !!"
+    echo "WHEN FINISHED DONT FORGET TO INVOKE: 'sc_switch_master.sh' !!"
 else
     rm public/system    
 fi
